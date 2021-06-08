dfbf56b9929607c2768f23997fcf3f68931e8ef6
kimnamsun <nsunny0908@naver.com>
2021-06-09 01:33:57 +0900

    Add : Nav 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import styled from 'styled-components';
+import { flexSet } from '@styles/Theme';
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Logo>SUNNYSHOP</Logo>
+      <Cart>
+        <i className="xi-cart-o" />
+        <Badge>
+          <Amount>1</Amount>
+        </Badge>
+      </Cart>
+    </Wrapper>
+  );
 };
 
 export default Nav;
+
+const Wrapper = styled.div`
+  ${flexSet('space-between', 'center')};
+  padding: 3rem;
+  max-height: 100px;
+  background-color: ${({ theme }) => theme.color.black};
+`;
+
+const Logo = styled.h1`
+  font-size: 30px;
+  color: ${({ theme }) => theme.color.white};
+`;
+
+const Cart = styled.button`
+  position: relative;
+  background-color: ${({ theme }) => theme.color.black};
+  font-size: 30px;
+  color: ${({ theme }) => theme.color.white};
+`;
+
+const Badge = styled.button`
+  position: absolute;
+  padding: 10px;
+  width: 15px;
+  height: 15px;
+  top: 0;
+  right: 0;
+  border-radius: 50%;
+  background-color: ${({ theme }) => theme.color.red};
+`;
+
+const Amount = styled.span`
+  position: absolute;
+  bottom: 10;
+  margin: -6px 0px 0px -2px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.color.white};
+`;
